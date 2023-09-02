d54f36050b9cf1a55e7d0c1fe251fb7a0ba3a166
Dmitry Blokhin <blokhindmitrijj@gmail.com>
2023-09-03 00:33:49 +0600

    Fix Lesson7/Task2 code

--- Lesson7/Task2/Task2/Task2.cpp
+++ Lesson7/Task2/Task2/Task2.cpp
@@ -59,9 +59,11 @@ void runBFS(int** adjacency_matrix, int verticies_number, int start_vertice)
 
     std::cout << "Verticies order: ";
 
-    while (queue_head < verticies_number)
+    for (int queue_head = 0; queue_head < verticies_number; ++queue_head)
     {
         int vertice_index = queue[queue_head];
+        if (vertice_index == 0) vertice_index = queue_head;
+
         if (!visited[vertice_index - 1])
         {
             visited[vertice_index - 1] = true;
@@ -75,10 +77,9 @@ void runBFS(int** adjacency_matrix, int verticies_number, int start_vertice)
                 visited[i] = true;
                 std::cout << i + 1 << " ";
                 queue_end++;
-                queue[queue_end] = i + 1;
+                if (queue_end < verticies_number) queue[queue_end] = i + 1;
             }
         }
-        queue_head++;
     }
     std::cout << std::endl;
 
@@ -88,16 +89,28 @@ void runBFS(int** adjacency_matrix, int verticies_number, int start_vertice)
 
 int main()
 {
-    std::ifstream in_file{ "input.txt" };
+    std::ifstream in_file{ "input4.txt" };
     int verticies_number;
     in_file >> verticies_number;
     int** adjacency_matrix = getAdjacencyMatrixFromFile(in_file, verticies_number);
     std::cout << "Adjacency matrix:\n";
     printTwoDimArray(adjacency_matrix, verticies_number, verticies_number);
 
-    std::cout << "Enter start vertice: ";
     int start_vertice;
-    std::cin >> start_vertice;
+    for (;;)
+    {
+        std::cout << "Enter start vertice: ";
+        std::cin >> start_vertice;
+        if (start_vertice > 0 && start_vertice <= verticies_number)
+        {
+            break;
+        }
+        else
+        {
+            std::cout << "Wrong vertice number! Try again.\n";
+        }
+    }
+    
     runBFS(adjacency_matrix, verticies_number, start_vertice);
     deleteTwoDimArray(adjacency_matrix, verticies_number, verticies_number);
     return 0;
