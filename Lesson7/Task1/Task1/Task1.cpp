8d19f364b5e3ec6475b8c7353cdd93839d0efd2f
Dmitry Blokhin <blokhindmitrijj@gmail.com>
2023-09-03 00:29:48 +0600

    Fix Lesson7/Task1 code

--- Lesson7/Task1/Task1/Task1.cpp
+++ Lesson7/Task1/Task1/Task1.cpp
@@ -49,7 +49,9 @@ void printTwoDimArray(int** array, int n, int m)
 void runDFS(int** adjacency_matrix, int verticies_number)
 {
     bool* visited = new bool[verticies_number] {false};
-
+    int* stack = new int[verticies_number] {};
+    int stack_top = -1;
+   
     std::cout << "Verticies order: ";
 
     for (int i = 0; i < verticies_number; ++i)
@@ -57,29 +59,39 @@ void runDFS(int** adjacency_matrix, int verticies_number)
         if (!visited[i])
         {
             visited[i] = true;
-            std::cout << i + 1 << " ";
-        }
+            stack_top++;
+            stack[stack_top] = i;
+            std::cout << stack[stack_top] + 1 << " ";
 
-        for (int j = 0; j < verticies_number; ++j)
-        {
-            if (adjacency_matrix[i][j] != 0 && !visited[j])
+            while (stack_top != -1)
             {
-               std::cout << j + 1 << " ";
-               visited[j] = true;
-               i = j;
-               j = 0;
+                for (int j = 0; j < verticies_number; ++j)
+                {
+                    if (adjacency_matrix[i][j] != 0 && !visited[j])
+                    {
+                        visited[j] = true;
+                        stack_top++;
+                        stack[stack_top] = j;
+                        std::cout << stack[stack_top] + 1 << " ";
+                        i = j;
+                        j = -1;
+                    }
+                }
+
+                i = stack[stack_top];
+                stack_top--;
             }
         }
-
     }
     std::cout << std::endl;
 
     delete[] visited;
+    delete[] stack;
 }
 
 int main()
 {
-    std::ifstream in_file { "input3.txt" };
+    std::ifstream in_file { "input5.txt" };
     int verticies_number;
     in_file >> verticies_number;
     int** adjacency_matrix = getAdjacencyMatrixFromFile(in_file, verticies_number);
